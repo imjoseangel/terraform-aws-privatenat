584eeb9f75e114a5cf1a3c7da7f9427c348221cb
imjoseangel <josea.munoz@gmail.com>
2022-03-22 19:48:24 +0100

    add region

--- variables.tf
+++ variables.tf
@@ -3,3 +3,9 @@ variable "vpc_name" {
   type        = list(string)
   default     = []
 }
+
+variable "region" {
+  description = "The region to use"
+  type        = string
+  default     = "eu-west-1"
+}
