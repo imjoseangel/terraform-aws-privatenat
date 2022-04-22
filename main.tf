283dc7f316f3afcfbec4f382efacec3ade457c64
imjoseangel <josea.munoz@gmail.com>
2022-04-23 00:08:35 +0200

    remove internet gw if not needed

--- main.tf
+++ main.tf
@@ -85,6 +85,7 @@ resource "aws_subnet" "external" {
 # Create Internet Gateways
 #-------------------------------
 resource "aws_internet_gateway" "main" {
+  count  = length(aws_subnet.external)
   vpc_id = data.aws_vpc.main[0].id
 
   tags = {
